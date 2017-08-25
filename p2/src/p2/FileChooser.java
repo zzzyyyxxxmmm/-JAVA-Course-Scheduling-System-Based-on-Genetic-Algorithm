0a6d4e361be7142e0f6f71aa6a98bea136e86208
zzzyyyxxxmmm <321114287@qq.com>
2017-08-25 07:24:58 -0500

    Add files via upload

---
+++ p2/src/p2/FileChooser.java
@@ -0,0 +1,46 @@
+package p2;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class FileChooser extends JFrame implements ActionListener{
+	JButton open=null;
+	public static void main(String[] args) {
+		new FileChooser();
+	}
+	public FileChooser(){
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES );
+		jfc.showDialog(new JLabel(), "选择");
+		File file=jfc.getSelectedFile();
+		if(file.isDirectory()){
+			System.out.println("文件夹:"+file.getAbsolutePath());
+		}else if(file.isFile()){
+			System.out.println("文件:"+file.getAbsolutePath());
+		}
+		System.out.println(jfc.getSelectedFile().getName());
+	}
+	public String ac(){
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES );
+		jfc.showDialog(new JLabel(), "选择");
+		File file=jfc.getSelectedFile();
+		if(file.isDirectory()){
+			System.out.println("文件夹:"+file.getAbsolutePath());
+		}else if(file.isFile()){
+			System.out.println("文件:"+file.getAbsolutePath());
+		}
+		System.out.println(jfc.getSelectedFile().getName());
+		return file.getAbsolutePath();
+	}
+
+}
